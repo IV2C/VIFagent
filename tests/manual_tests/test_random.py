3c83dd61d8782df2a9cc918717c22a65acc5731b
charly reux <charlyreux@gmail.com>
2025-05-13 12:13:08 +0200

    oracle implemented, added tests for identify

--- tests/manual_tests/test_random.py
+++ tests/manual_tests/test_random.py
@@ -1,45 +1,63 @@
-chimp_tex = open("chimpanzee.tex").read()
+import json
+import os
+import re
+from openai import OpenAI
+from build.lib.vif_agent.prompt import FEATURE_IDENTIFIER_PROMPT
+from vif_agent.modules.identification.utils import get_boxes
+from vif_agent.renderer.tex_renderer import TexRenderer
+from vif_agent.utils import encode_image
 
-print(chimp_tex[214:270])
+renderer = TexRenderer()
+monkey_code = open("tests/resources/monkey.tex", "r").read()
+image = renderer.from_string_to_image(monkey_code)
+
+client = OpenAI(
+    api_key=os.environ.get("GOOGLE_API_KEY"),
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+model = "gemini-2.0-flash"
+temperature = 0.3
 
 
+# getting feature
+encoded_image = encode_image(image=image)
+response = client.chat.completions.create(
+    model=model,
+    temperature=temperature,
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": FEATURE_IDENTIFIER_PROMPT,
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {"url": f"data:image/jpeg;base64,{encoded_image}"},
+                },
+            ],
+        }
+    ],
+)
+pattern = r"```(?:\w+)?\n([\s\S]+?)```"
+search_match = re.search(pattern, response.choices[0].message.content)
 
-""" 
 
-pink monkey face
-[(1473, 1490)]
-[(645, 719)]
-[(1164, 1223)]
-left black monkey eye
-[(1164, 1223)]
-[(1473, 1490)]
-[(721, 795)]
-right black monkey eye
-[(1473, 1490)]
-[(1164, 1223)]
-[(873, 1141)]
-pink monkey nose
-[(1473, 1490)]
-[(645, 719)]
-[(1225, 1306)]
-pink monkey mouth
-[(1473, 1490)]
-[(645, 719)]
-[(1308, 1385)]
-left pink monkey ear
-[(1473, 1490)]
-[(272, 379)]
-[(214, 270)]
-right pink monkey ear
-[(1473, 1490)]
-[(381, 644)]
-[(534, 641)]
-black monkey head outline
-[(1473, 1490)]
-[(645, 719)]
-[(1164, 1223)]
-light blue background circle
-[(1473, 1490)]
-[(645, 719)]
-[(1164, 1223)]
-"""
+features_match = search_match.group(1)
+features = json.loads(features_match)
+print("Features")
+print(features)
+
+boxes = get_boxes(
+    client=client, image=image, model=model, temperature=temperature, features=features
+)
+
+
+with open("tests/resources/mapped_code/monkey_boxes", "w") as mkbx:
+    json.dump(boxes, mkbx)
+
+for box in boxes:
+    print("----------------------")
+    print(box["label"])
+    print(box["box_2d"])
---
+++ tests/test_mapped_code/test_mapped_code_identify.py
@@ -0,0 +1,55 @@
+import unittest
+import json
+from PIL import Image
+import numpy as np
+from vif_agent.feature import CodeImageMapping
+import vif_agent.modules.identification.mapping
+
+
+class TestMappedCodeIdentify(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.monkey_features = {
+            "features": [
+                "light blue background circle",
+                "brown monkey head outline",
+                "pink monkey face",
+                "left brown monkey ear",
+                "right brown monkey ear",
+                "left pink monkey ear",
+                "right pink monkey ear",
+                "left black monkey eye",
+                "right black monkey eye",
+                "left pink monkey nostril",
+                "right pink monkey nostril",
+                "pink curved monkey mouth",
+            ]
+        }
+
+        cls.monkey_code = open("tests/resources/monkey.tex", "r").read()
+        cls.monkey_image = Image.open("tests/resources/mapped_code/monkey.png")
+
+        def new_get_boxes(image, client, features, model, temperature):
+            return json.load(open("tests/resources/mapped_code/monkey_boxes.json", "r"))
+
+        vif_agent.modules.identification.mapping.get_boxes = new_get_boxes
+
+    def test_identify_simple(self):
+
+        box_id_module = (
+            vif_agent.modules.identification.mapping.BoxIdentificationModule(
+                client=None,
+                model=None,
+            )
+        )
+
+        mapped_code = box_id_module.identify(
+            base_image=self.monkey_image,
+            code=self.monkey_code,
+            features=self.monkey_features,
+        )
+
+        self.assertEqual(
+            mapped_code.feature_map["left black monkey eye"][0][0].spans, [(1383, 1430)]
+        )
