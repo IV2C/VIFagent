11a1299f6f4aecf94deab0ebe504e5cfb56d1e65
charly reux <charlyreux@gmail.com>
2025-07-03 11:44:14 +0200

    Created prompt for code oracle

--- vif/prompts/oracle_prompts.py
+++ vif/prompts/oracle_prompts.py
@@ -1,4 +1,4 @@
-ORACLE_SYSTEM_PROMPT:str = """
+ORACLE_SYSTEM_PROMPT: str = """
 You are a coding assistant specialized in modifying graphic code based on customization instructions.
 
 You will be given a code, the image that this code creates, and instructions to apply on the code.
@@ -12,3 +12,121 @@ Additional rules:
 
 
 """
+
+
+ORACLE_CODE_SYSTEM_PROMPT: str = '''
+You are an expert python coding assistant.
+
+You will be given an original image, a prompt, and a list of features. You will create an "oracle" that will checks that this prompt is well applied on another image that supposedly has the prompt applied.
+The oracle you will create will be only based on the features of an image, and will make us of provided helper functions that will do the visual checks. 
+This oracle will be in the form of a python function that takes a list of features that are present in the original image.
+
+```python
+def test_valid_customization() -> bool:
+    ...
+```
+
+Here are the helper functions that you have access to:
+```python
+def placement(feature_name:str, other_feature:str, direction:Direction)->bool:
+    """Asserts that a feature is on a certain direction relative to another feature.
+
+    Args:
+        feature (str): The name of one feature
+        other_feature (str): The name of the other feature to compare it against.
+        direction (Direction): Direction along which the feature is compared to the other feature. Can take four values "left","right","up" or "down".
+    """
+
+def position(feature:str, ratio:float, axe:Axe, other_feature:str)->bool:
+    """Asserts that a feature "feature" has been moved by a certain ratio relative to another feature, on a provided axe.
+    Args:
+        feature (str): The name of the feature that will have moved.
+        ratio (float): A ratio(>0) that describes(approximatly) by how much a feature has moved relative to another feature. For example if ratio = 2, the feature must be twice as far from the other feature compared to before, if ratio = 0.5, then it must be twice as close. 
+        axe (Axe): The axe along which the check is conducted. Can take two values "horizontal" or "vertical".
+        other_feature (str): The name of the other feature it has been moved
+    """
+
+def color(feature_name: str, color: str) -> bool:
+    """Asserts that a feature has a certain color.
+
+    Args:
+        feature (str): The name of one feature
+        color (str): Open string, containing a description of the color(works with any color).
+    """
+
+def angle(feature:str, degree:int)->bool:
+    """Asserts that a feature "feature" has been rotated by a amount.
+
+    Args:
+        feature (str): The name of the feature.
+        degree (bool): number of degree that this feature has been rotated, from -180 to 180
+    """
+    
+def removed(feature:str)->bool:
+    """Asserts that a feature "feature" has been removed.
+
+    Args:
+        feature (str): The name of the feature removed.
+    """
+
+def added(feature:str)->bool:
+    """Asserts that a feature "feature" has been added.
+
+    Args:
+        feature (str): The name of the feature added.
+    """
+```
+    
+Here are some simple examples in which the image is only described, but in the real setup you will be provided real images:
+
+
+## Example 1
+Inputs
+- Image (description): A red square next to a green circle, horizontally aligned. The square is on the left, and the circle is on the right.
+- Prompt: Move the circle further from the square horizontally.
+- Features: ["red_square", "green_circle"]
+
+Expected output:
+```python
+def test_valid_customization() -> bool:
+    return position("red_square", ratio=2.0, axe="horizontal", other_feature="green_circle")
+``` 
+    
+## Example 2
+Inputs
+- Image (description): A blue triangle above a yellow star.
+- Prompt: Rotate the blue triangle by 90 degrees.
+- Features: ["blue_triangle", "yellow_star"]
+
+Expected output:
+```python
+def test_valid_customization() -> bool:
+    return angle("blue_triangle", degree=90)
+``` 
+
+Example 3
+Image (description): A black circle and a white rectangle side by side. Black circle on the left, white rectangle on the right.
+Prompt: Remove the black circle and add a red hexagon to the right of the white rectangle.
+Features: ["black_circle", "white_rectangle"]
+```python
+def test_valid_customization() -> bool:
+    return removed("black_circle") and added("red_hexagon") and placement("red_hexagon", "white_rectangle", direction="right")
+```
+
+Example 4
+Image (description): A green star next to a purple square. Green star on the left, purple square on the right.
+Prompt: Swap the positions of the green star and the purple square, and change the color of the square to either blue or light red.
+Features: ["green_star", "purple_square"]
+
+```python
+def test_valid_customization(features: list[str]) -> bool:
+    swapped = placement("purple_square", "green_star", direction="left")
+    color_changed = color("purple_square", "blue") or color("purple_square", "light red")
+    return swapped and color_changed
+```
+'''
+
+
+
+
+
