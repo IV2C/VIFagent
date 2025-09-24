bb666e2b46ea5eef9155027ff32b18c9f6451fed
charly reux <charlyreux@gmail.com>
2025-09-24 16:05:56 +0200

    update data format for baseline

---
+++ vif/baselines/Iterative/ite_baseline.py
@@ -0,0 +1,29 @@
+
+from openai import Client
+
+
+class TexIteBaseline:
+    def __init__(
+        self,
+        *,
+        edit_client: Client,
+        edit_model: str,
+        edit_temperature: float,
+        feedback_client: Client,
+        feedback_model: str,
+        feedback_temperature: float,
+        max_iterations: int,
+        **kwargs,
+    ):
+        super().__init__()
+        self.edit_client = edit_client
+        self.edit_model = edit_model
+        self.edit_temperature = edit_temperature
+        self.feedback_client = feedback_client
+        self.feedback_model = feedback_model
+        self.feedback_temperature = feedback_temperature
+        self.max_iteration = max_iterations
+
+    def customize(self, code: str, instruction: str) -> CustomizationOutput:
+
+        pass
