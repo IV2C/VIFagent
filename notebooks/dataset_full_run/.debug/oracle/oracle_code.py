c78f198f77c8d9ee2485a3bdf8f99482531b4c49
charly reux <charlyreux@gmail.com>
2025-10-24 13:57:48 +0200

    re-organizing and image computation for dataset

--- notebooks/dataset_full_run/.debug/oracle/oracle_code.py
+++
@@ -1,6 +0,0 @@
-def test_valid_customization() -> bool:
-    """
-    Asserts that the square has been rotated by 45 degrees clockwise.
-    """
-    # Clockwise rotation is represented by a negative angle.
-    return angle("blue square", degree=-45)
